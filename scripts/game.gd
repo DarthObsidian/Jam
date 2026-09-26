6ad63823cd3c2abf456866607f34449392d2f34b
Matthew Meyers <DarthObsidian@users.noreply.github.com>
2026-09-26 09:32:09 -0600

    made game pause when focus is lost.

--- scripts/game.gd
+++ scripts/game.gd
@@ -21,6 +21,13 @@ func _ready() -> void:
 	_reset()
 
 
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_APPLICATION_FOCUS_OUT:
+		get_tree().paused = true
+	elif what == NOTIFICATION_APPLICATION_FOCUS_IN:
+		get_tree().paused = false
+
+
 func _add_points() -> void:
 	var oldTotal = totalPoints
 	totalPoints += POINTS * $Combo.get_combo_multiplyer()
